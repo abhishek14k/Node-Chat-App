69302b1a2de0e7cb1764792ae95e7004b02065ff
Abhishek <abhishekramakrishna1995@gmail.com>
2018-12-24 10:11:09 +0530

    Added newMessage and createMessage events

---
+++ public/js/index.js
@@ -0,0 +1,18 @@
+var socket = io();
+
+socket.on('connect', () => {
+  console.log('Connected to server!');
+
+  socket.emit('createMessage', {
+    from: '14K!',
+    text: 'Aprilia RSV4 RR'
+  });
+});
+
+socket.on('disconnect', () => {
+  console.log('Disconnected from server!');
+});
+
+socket.on('newMessage', function(message) {
+  console.log('newMessage', message);
+});
