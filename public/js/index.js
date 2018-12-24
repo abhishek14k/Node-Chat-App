db242f50893ba3864f9772a11090d9fafce19be4
Abhishek <abhishekramakrishna1995@gmail.com>
2018-12-24 12:19:02 +0530

    Emit message on create Message

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,11 @@ var socket = io();
 
 socket.on('connect', () => {
   console.log('Connected to server!');
-
-  socket.emit('createMessage', {
-    from: '14K!',
-    text: 'Aprilia RSV4 RR'
-  });
+  // 
+  // socket.emit('createMessage', {
+  //   from: '14K!',
+  //   text: 'Aprilia RSV4 RR'
+  // });
 });
 
 socket.on('disconnect', () => {
--- server/server.js
+++ server/server.js
@@ -14,15 +14,19 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected!');
 
-  socket.emit('newMessage', {
-    from: 'Tobi',
-    text: 'Pain',
-    createdAt: 1507
-  });
+  // socket.emit('newMessage', {
+  //   from: 'Tobi',
+  //   text: 'Pain',
+  //   createdAt: 1507
+  // });
 
   socket.on('createMessage', (message) => {
     console.log('createMessage', message);
-    // io.emit('newMessage', )
+    io.emit('newMessage', {
+      from: message.from,
+      text: message.text,
+      createdAt: new Date().getTime()
+    })
   });
 
   socket.on('disconnect', () => {
